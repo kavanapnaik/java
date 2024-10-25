64b83f0956e6a320f9d7b32ba9fdf5ca32590e63
kavanapnaik <kavanakavu1436.com>
2024-10-25 12:48:57 +0530

    core java

---
+++ Person.java
@@ -0,0 +1,27 @@
+class Person
+{
+	public static void main(String[] args)
+	{
+		String name="Kavana";
+		String email="kavanapnaik1436@gmail.com";
+		String phone="8088781616";
+		String blood_group="A+";
+		String address="Navule, Shimoga";
+		String qualification="MCA";
+		String marital_status="Unmarried";
+		int age=24;
+		Float height=5.3f;
+		int weight=49;
+		long Aadhar=81805540;
+		System.out.println("Name:" +name);
+		System.out.println("Email:" +email);
+		System.out.println("Phone:" +phone);
+		System.out.println("Blood_group:" +blood_group);
+		System.out.println("Address:" +address);
+		System.out.println("Qualification:" +qualification);
+		System.out.println("Marital_status:" +marital_status);
+		System.out.println("Age:" +age);
+		System.out.println("Height:" +height);
+		System.out.println("Weight:" +weight);
+	}
+}
